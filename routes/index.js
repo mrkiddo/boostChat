2fa90920059bfa4c10b64e1b61388bbed9213368
Jiawei Luo <weiwei100fun@hotmail.com>
2017-01-25 22:26:07 -0500

    UI refine for login page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res, next) {
     res.render('login', { title: 'boostChat - Sign In'});
 });
 
+router.get('/login', function (req, res, next) {
+    res.render('login', { title: 'boostChat - Sign In'});
+});
+
 router.get('/chat', function (req, res, next) {
   res.render('chat', { title: 'boostChat' });
 });
--- util/src/login.js
+++ util/src/login.js
@@ -8,5 +8,9 @@ var componentLogin = require('./components/com.login');
 Vue.component('login-panel', componentLogin);
 
 var vm = new Vue({
+    ready: function () {
+        var loginContainer = $(this.$el).find('.container');
+        loginContainer.css('height', window.screen.availHeight + 'px');
+    },
     el: '.user-auth'
 });
